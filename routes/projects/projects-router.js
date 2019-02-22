cbf9c3ff5bf8a8391e89e7cdf9a178100d97f13f
Modesto Tamayo <modesto.t.tamayo@gmail.com>
2019-02-22 11:56:57 -0600

    Added the end-point for getting a project by it's id

--- routes/projects/projects-router.js
+++ routes/projects/projects-router.js
@@ -14,4 +14,19 @@ router.get('/', async (req, res) => {
     }
 });
 
+router.get('/:id', async (req, res) => {
+    try {
+        const project = await Projects.getProjectById(req.params.id);
+
+        if(project) {
+            res.status(200).json(project);
+        } else {
+            res.status(404).json({ error: 'That Project id does not exist' });
+        }
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({ error: 'The Project information could not be retrieved' });
+    }
+});
+
 module.exports = router;
